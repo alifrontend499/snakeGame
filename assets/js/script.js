c355774b8240be3001b4f08ba11d9dc79b617915
alifrontend499 <alifrontend499@gmail.com>
2018-06-03 14:10:00 +0530

    home changes

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,61 +1,71 @@
 // SNAKE GAME STARTS
 window.onload = function () {
     "use strict";
-    // let left = 0;
-    // let right = 0;
-    // let top = 0;
-    // let bottom = 0;
-    // let leftPosInt, rightPosInt, topPosInt, bottomPosInt;
+    let left = 0;
+    let right = 0;
+    let top = 0;
+    let bottom = 0;
+
+    let leftPosInt, rightPosInt, topPosInt, bottomPosInt;
     // let elem = document.querySelector('.snake-game-inner');
-    // let leftBtn = document.querySelector('.left');
-    // let rightBtn = document.querySelector('.right');
-    // let topBtn = document.querySelector('.top');
-    // let bottomBtn = document.querySelector('.bottom');
-    // let slideSpeed = 100;
+    let leftBtn = document.querySelector('.left');
+    let rightBtn = document.querySelector('.right');
+    let topBtn = document.querySelector('.top');
+    let bottomBtn = document.querySelector('.bottom');
 
-    // function leftPos() {
-    //     elem.style.cssText = "left: " + left + "px; top: " + top + "px; transform: rotate(0deg)";
-    //     left += 10;
-    // }
-    // function rightPos() {
-    //     elem.style.cssText = "left: " + left + "px; top: " + top + "px; transform: rotate(180deg)";
-    //     left -= 10;
-    // }
-    // function topPos() {
-    //     elem.style.cssText = "top: " + top + "px; left: " + left + "px; transform-origin: center; transform: rotate(-90deg)";
-    //     if (top !== 0) {
-    //         top -= 10;
-    //     }
-    // }
-    // function bottomPos() {
-    //     elem.style.cssText = "top: " + top + "px; left: " + left + "px; transform-origin: center; transform: rotate(90deg)";
-    //     top += 10;
-    // }
+    let sgInner = document.querySelector('.snake-game-inner');
+    let snake = document.querySelector('.snake-game-inner .snake');
+    let snakeOffsetLeft = snake.offsetLeft - 2;
+    let snakeOffsetTop = snake.offsetTop - 2;
+
+    let slideSpeed = 100;
+
+    function leftPos() {
+        snake.style.cssText = "left: " + left + "px; top: " + top + "px; transform: rotate(0deg)";
+        left += 10;
+    }
+    function rightPos() {
+        snake.style.cssText = "left: " + left + "px; top: " + top + "px; transform: rotate(180deg)";
+        left -= 10;
+    }
+    function topPos() {
+        snake.style.cssText = "top: " + top + "px; left: " + left + "px; transform-origin: center; transform: rotate(-90deg)";
+        if (top !== 0) {
+            top -= 10;
+        }
+    }
+    function bottomPos() {
+        snake.style.cssText = "top: " + top + "px; left: " + left + "px; transform-origin: center; transform: rotate(90deg)";
+        top += 10;
+    }
 
 
-    // leftBtn.addEventListener("click", function () {
-    //     leftPosInt = setInterval(leftPos, slideSpeed);
-    //     clearInterval(rightPosInt);
-    //     clearInterval(topPosInt);
-    //     clearInterval(bottomPosInt);
-    // });
-    // rightBtn.addEventListener("click", function () {
-    //     rightPosInt = setInterval(rightPos, slideSpeed);
-    //     clearInterval(leftPosInt);
-    //     clearInterval(topPosInt);
-    //     clearInterval(bottomPosInt);
-    // });
-    // topBtn.addEventListener("click", function () {
-    //     topPosInt = setInterval(topPos, slideSpeed);
-    //     clearInterval(leftPosInt);
-    //     clearInterval(rightPosInt);
-    //     clearInterval(bottomPosInt);
-    // });
-    // bottomBtn.addEventListener("click", function () {
-    //     bottomPosInt = setInterval(bottomPos, slideSpeed);
-    //     clearInterval(leftPosInt);
-    //     clearInterval(rightPosInt);
-    //     clearInterval(topPosInt);
-    // });
+    leftBtn.addEventListener("click", function () {
+        leftPosInt = setInterval(leftPos, slideSpeed);
+        clearInterval(rightPosInt);
+        clearInterval(topPosInt);
+        clearInterval(bottomPosInt);
+    });
+    rightBtn.addEventListener("click", function () {
+        rightPosInt = setInterval(rightPos, slideSpeed);
+        clearInterval(leftPosInt);
+        clearInterval(topPosInt);
+        clearInterval(bottomPosInt);
+    });
+    topBtn.addEventListener("click", function () {
+        topPosInt = setInterval(topPos, slideSpeed);
+        clearInterval(leftPosInt);
+        clearInterval(rightPosInt);
+        clearInterval(bottomPosInt);
+    });
+    bottomBtn.addEventListener("click", function () {
+        bottomPosInt = setInterval(bottomPos, slideSpeed);
+        clearInterval(leftPosInt);
+        clearInterval(rightPosInt);
+        clearInterval(topPosInt);
+    });
+    
+    
+    
 }
 // SNAKE GAME FINISH
